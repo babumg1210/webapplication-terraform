0ad34b2b8d2826c81e0b343eae3953241a9d50fc
babumg1210 <babumg1210@outlook.com>
2024-12-29 15:30:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,4 @@ module "webapp" {
   location              = var.location
   app_service_plan_name = var.app_service_plan_name
   web_app_name          = var.web_app_name
-  sku_tier              = var.sku_tier
-  sku_size              = var.sku_size
-  sku_capacity          = var.sku_capacity
 }
