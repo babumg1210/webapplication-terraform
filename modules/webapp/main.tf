0b3c61a707356cfb471d8574643ef5280c2e5c64
babumg1210 <babumg1210@outlook.com>
2024-12-29 15:01:22 +0530

    Update main.tf

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_service_plan" "this" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Windows"  # Optional, can be omitted if not needed
   sku {
     tier     = var.sku_tier
     size     = var.sku_size
@@ -18,7 +19,7 @@ resource "azurerm_app_service" "this" {
   name                = var.web_app_name
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-  app_service_plan_id = azurerm_app_service_plan.this.id
+  app_service_plan_id = azurerm_service_plan.this.id  # Corrected reference
 
   app_settings = {
     "WEBSITE_NODE_DEFAULT_VERSION" = "14"  # Example setting for Node.js
