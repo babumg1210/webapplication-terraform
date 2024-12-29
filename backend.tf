42b6e23e39d05a405d7ea9d10e13dd59f035a6a9
babumg1210 <babumg1210@outlook.com>
2024-12-29 14:36:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,9 +4,9 @@ terraform {
     storage_account_name  = "ecommercestorage1210"
     container_name        = "webapp"
     key                   = "terraform.tfstate"
-    #subscription_id       = "ae1b5111-4f4f-477d-b4d4-419fd8cbc789"
-    #tenant_id             = "c7de2ec6-e7a9-41e7-99e8-e6ff52f3a8d0"
-    #client_id             = "419aeb43-db49-4a70-8239-868d17b24283"   # Replace with your actual client ID
-    #client_secret         = "IWs8Q~peHExZd3LbMfGfut0cpj9p8Fgltt-vbcKO"  # Replace with your actual client secret
+    subscription_id       = "ae1b5111-4f4f-477d-b4d4-419fd8cbc789"
+    tenant_id             = "c7de2ec6-e7a9-41e7-99e8-e6ff52f3a8d0"
+    client_id             = "100d268f-26db-4459-94cc-b752d3025b61"   # Replace with your actual client ID
+    client_secret         = "c7de2ec6-e7a9-41e7-99e8-e6ff52f3a8d0"  # Replace with your actual client secret
   }
 }
