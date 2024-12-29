0f2ab7b7e361dae1a40150f9c139167c29bd5e42
babumg1210 <babumg1210@outlook.com>
2024-12-29 13:51:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id       = "ae1b5111-4f4f-477d-b4d4-419fd8cbc789"
+  tenant_id             = "c7de2ec6-e7a9-41e7-99e8-e6ff52f3a8d0"
+  client_id             = "419aeb43-db49-4a70-8239-868d17b24283"   # Replace with your actual client ID
+  client_secret         = "IWs8Q~peHExZd3LbMfGfut0cpj9p8Fgltt-vbcKO" 
 }
 
 module "webapp" {
